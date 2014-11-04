b309e84f6aa0497d7fd810de6917d5e7e93015e7
CollinSmith <collinsmith1990@gmail.com>
2014-11-03 19:53:08 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Radix_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var testArray = new int[1000];
+            for(int i = 999; i >= 0; i--){
+                testArray[i] = rand.Next(10000);
+            }
+
+            Sorter sort = new Sorter();
+            var timeIt = new Stopwatch();
+            timeIt.Start();
+            sort.Radix(testArray);
+            timeIt.Stop();
+
+            int count = 0;
+            foreach(int i in testArray){
+                if(count % 10 == 0){
+                    Console.WriteLine();
+                    count = 0;
+                }
+                Console.Write(i + ", ");
+                count ++;
+            }
+            Console.WriteLine(timeIt.Elapsed);
+        }
+    }
+}
---
+++ Sorter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Radix_Sort
+{
+    class Sorter
+    {
+        public int[] Radix(int[] input){
+            var numbers = new Queue<int>(input);
+            var buckets = InitializeArray<Queue<int>>(10);
+            int m = 10;
+            int n = 1;
+            for(int i = numbers.Max().ToString().Length; i > 0; i--){
+                while(numbers.Count > 0){
+                    buckets[numbers.Peek()%m/n].Enqueue(numbers.Dequeue());
+                }
+                foreach(Queue<int> bucket in buckets){
+                    while(bucket.Count > 0){
+                        numbers.Enqueue(bucket.Dequeue());
+                    }
+                }
+                m *= 10;
+                n *= 10;
+            }
+            return numbers.ToArray();
+        }
+        private static T[] InitializeArray<T>(int length) where T : new(){
+            T[] array = new T[length];
+            for (int i = 0; i < length; ++i){
+                array[i] = new T();
+            }
+
+            return array;
+        }
+    }
+}
